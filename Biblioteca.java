0cccea64663b1b60a32ddcba3778d2d19955f0e5
crisilto <cristinasilvestre1708@gmail.com>
2023-11-17 21:54:20 +0100

    fixes

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,66 +1,169 @@
 package biblioteca;
 
+import java.util.Scanner;
+
 public class Biblioteca {
     private Libro[] libros;
     private int numLibros;
 
-    public Biblioteca(int tamaño){
+    public Biblioteca(int tamaño) {
         libros = new Libro[tamaño];
         numLibros = 0;
     }
+
+    public void crearLibro() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el título del libro: ");
+        String titulo = sc.nextLine();
+        System.out.println("Ingrese el autor del libro: ");
+        String autor = sc.nextLine();
+        System.out.println("Ingrese el ISBN del libro: ");
+        String isbn = sc.nextLine();
+        System.out.println("Ingrese el género del libro: ");
+        String genero = sc.nextLine();
+        System.out.println("Ingrese la editorial del libro: ");
+        String editorial = sc.nextLine();
+        System.out.println("Ingrese el año de publicación del libro: ");
+        int añoPublicacion = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el idioma del libro: ");
+        String idioma = sc.nextLine();
+        System.out.println("Ingrese las páginas del libro: ");
+        int paginas = sc.nextInt();
+        sc.nextLine();
+        System.out.println("¿Ha leído el libro?: ");
+        boolean leido = sc.nextBoolean();
+
+        Libro nuevoLibro = new Libro(titulo, autor, isbn, genero, editorial, añoPublicacion, idioma, paginas, leido);
+        añadirLibro(nuevoLibro);
+
+        System.out.println("Libro creado y añadido a la Biblioteca con éxito.");
+    }
+
     //Añadir un libro al array libros[]
-    public void añadirLibro (Libro libro){
-        if(numLibros < libros.length){
+    public void añadirLibro(Libro libro) {
+        if (numLibros < libros.length) {
             libros[numLibros] = libro;
             numLibros++;
-        }else{
+        } else {
             System.out.println("No hay espacio para más libros.");
         }
     }
+
     //Eliminar un libro del array libros[]
-    public void eliminarLibro (String isbn){
-        for(int i = 0; i < numLibros; i++){
-            if(libros[i].getIsbn().equalsIgnoreCase(isbn)){
-                for(int j = i; j < numLibros - 1; j++){
-                    libros[j] = libros[j+1];
+    public void eliminarLibro(String titulo) {
+        for (int i = 0; i < numLibros; i++) {
+            if (libros[i].getTitulo().trim().equalsIgnoreCase(titulo.trim())) {
+                for (int j = i; j < numLibros - 1; j++) {
+                    libros[j] = libros[j + 1];
                 }
                 numLibros--;
                 return;
             }
         }
-        System.out.println("Libro eliminado.");
+
     }
+
     //Buscar un libro en el array libros[]
-    public void buscarLibro (String titulo){
-        for(int i = 0; i < numLibros; i++){
-            System.out.println("Comparando con: "+libros[i].getTitulo());
-            if(libros[i].getTitulo().trim().equalsIgnoreCase(titulo.trim())){
+    public void buscarLibro(String titulo) {
+        for (int i = 0; i < numLibros; i++) {
+            if (libros[i].getTitulo().trim().equalsIgnoreCase(titulo.trim())) {
                 System.out.println("Libro encontrado: " + libros[i].getTitulo());
                 return;
             }
         }
         System.out.println("Libro no encontrado.");
     }
+
     //Mostrar todos los libros del array libros[] de dentro de la Biblioteca
-    public void mostrarBiblioteca(){
+    public void mostrarBiblioteca() {
         System.out.println("Libros disponibles en La Biblioteca: ");
-        for(int i = 0; i < numLibros; i++){
-            System.out.println(libros[i].getCodigo() +": "+libros[i].getTitulo());
+        for (int i = 0; i < numLibros; i++) {
+            System.out.println(libros[i].getCodigo() + ": " + libros[i].getTitulo());
         }
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Biblioteca biblioteca = new Biblioteca(5);
 
-        Libro libro1 = new Libro("Harry Potter 1", "JK Rowling", "L001", "Fantasía", "Salamandra", 1999, "Español", 256,true);
+        Libro libro1 = new Libro("Harry Potter 1", "JK Rowling", "L001", "Fantasía", "Salamandra", 1999, "Español", 256, true);
         Libro libro2 = new Libro("Crepúsculo", "Stephanie Mayer", "L002", "Fantasía", "Alfaguara", 2006, "Español", 512, false);
-
         biblioteca.añadirLibro(libro1);
-        biblioteca.mostrarBiblioteca();
-        biblioteca.buscarLibro("Crepúsculo"); //No encontrado
-        biblioteca.añadirLibro(libro2); //Lo añadimos
-        biblioteca.mostrarBiblioteca();
-        biblioteca.buscarLibro("Crepúsculo"); //Encontrado
+        biblioteca.añadirLibro(libro2);
 
+        boolean salir = false; //Booleano para controlar menú
+        System.out.println("Bienvenido/a a la Biblioteca.\nSeleccione una opción (6 para salir): ");
+        while (!salir) {
+            System.out.println(
+                    "1. Mostrar Biblioteca" +
+                            "\n2. Buscar Libro" +
+                            "\n3. Añadir Libro" +
+                            "\n4. Eliminar libro" +
+                            "\n5. Marcar libro como leído" +
+                            "\n6. Salir");
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("---------------------------");
+                    biblioteca.mostrarBiblioteca();
+                    sc.nextLine();
+                    System.out.println("---------------------------");
+                    break;
+                case 2:
+                    System.out.println("---------------------------");
+                    System.out.println("Introduce el nombre del libro que desea bucar:");
+                    sc.nextLine();
+                    String nombreBuscar = sc.nextLine();
+                    biblioteca.buscarLibro(nombreBuscar);
+                    System.out.println("---------------------------");
+                    break;
+                case 3:
+                    System.out.println("---------------------------");
+                    biblioteca.crearLibro();
+                    sc.nextLine();
+                    System.out.println("---------------------------");
+                    break;
+                case 4:
+                    System.out.println("---------------------------");
+                    System.out.println("Introduce el nombre del libro que desea eliminar:");
+                    sc.nextLine();
+                    String nombreEliminar = sc.nextLine();
+                    biblioteca.eliminarLibro(nombreEliminar);
+                    System.out.println("---------------------------");
+                    break;
+                case 5:
+                    System.out.println("---------------------------");
+                    System.out.println("¿Qué libro desea marcar como leído? Indique el título.");
+                    sc.nextLine();
+                    String nombreLeido = sc.nextLine();
+                    boolean libroEncontrado = false;
+                    for (int i = 0; i < biblioteca.numLibros; i++) {
+                        Libro libro = biblioteca.libros[i];
+                        if (libro.getTitulo().equalsIgnoreCase(nombreLeido)) {
+                            libro.marcarComoLeido(true);
+                            libroEncontrado = true;
+                            System.out.println("Libro marcado como leído correctamente.");
+                            break;
+                        }
+                    }
+                    if (!libroEncontrado) {
+                        System.out.println("Libro no encontrado, verifique el título.");
+                    }
+                    System.out.println("---------------------------");
+                    break;
+                case 6:
+                    System.out.println("---------------------------");
+                    System.out.println("Hasta luego.");
+                    salir = true;
+                    System.out.println("---------------------------");
+                    break;
+                default:
+                    System.out.println("Introduce un número válido entre el 1 y el 6.");
+                    break;
+            }
+        }
+        sc.close();
     }
 }
--- Libro.java
+++ Libro.java
@@ -1,10 +1,11 @@
 package biblioteca;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Libro {
     private static int contadorCodigo = 0;
-    private int codigo;
+    private final int codigo; //Los códigos generados (gracias al contador static) serán finales
     private String titulo;
     private String autor;
     private String isbn;
@@ -38,72 +39,77 @@ public class Libro {
         this.isbn = isbn;
         this.genero = genero;
         this.editorial = editorial;
-        this.añoPublicacion = añoPublicacion;
+        //Validación en el dato añoPublicación
+        if(añoPublicacion > 0 && añoPublicacion <= LocalDateTime.now().getYear()){
+            this.añoPublicacion = añoPublicacion;
+        }else{
+            this.añoPublicacion = 0; //Año predeterminado si el año es menos a 0 o mayor al año actual
+        }
         this.idioma = idioma;
-        this.paginas = paginas;
+        //Validación en el dato paginas
+        if(paginas > 0){
+            this.paginas = paginas;
+        }else{
+            this.paginas = 0; //Valor predeterminado si el número de páginas es inferior a 0
+        }
         this.leido = leido;
     }
     public int getCodigo(){
-        return codigo;
+        return codigo; //No tiene setter puesto que se trata de una constante
     }
-
-    //Permitir establecer título al recibir un parámetro
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
-    //Devolver un string con el valor del título
     public String getTitulo() {
-        return titulo + "\n";
+        return titulo;
     }
-
-    //Permitir establecer autor al recibir un parámetro
     public void setAutor(String autor) {
         this.autor = autor;
     }
-
-    //Devolver un string con el valor del autor
     public String getAutor() {
-        return "El autor es " + autor + "\n";
+        return autor;
     }
-
-    //Permitir establecer isbn al recibir un parámetro
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-
-    //Devolver un string con el valor del isbn
     public String getIsbn() {
         return "El ISBN es " + isbn + "\n";
     }
-
-    //Permitir establecer genero al recibir un parámetro
     public void setGenero(String genero) {
         this.genero = genero;
     }
-
-    //Devolver un string con el valor del genero
     public String getGenero() {
-        return "El género es " + genero + "\n";
+        return genero;
     }
-
-    //Getters de las propiedades/los atributos ya inicializados
+    public void setEditorial(String editorial){
+        this.editorial = editorial;
+    }
     public String getEditorial() {
-        return "La editorial es " + editorial + "\n";
+        return editorial;
     }
-
-    public String getAñoPublicacion() {
-        return "El año de publicación es " + añoPublicacion + "\n";
+    public void setAñoPublicacion(int añoPublicacion){
+        this.añoPublicacion = añoPublicacion;
     }
-
+    public int getAñoPublicacion() {
+        return añoPublicacion;
+    }
+    public void setIdioma(String idioma){
+        this.idioma = idioma;
+    }
     public String getIdioma() {
-        return "El idioma es " + idioma + "\n";
+        return idioma;
     }
-    public String getPaginas(){
-        return "Tiene "+paginas+" páginas";
+    public void setPaginas(int paginas){
+        this.paginas = paginas;
     }
-    public String getLeido(){
-        return leido ? "Si" : "No";
+    public int getPaginas(){
+        return paginas;
+    }
+    public void setLeido(boolean leido){
+        this.leido = leido;
+    }
+    public boolean getLeido(){
+        return leido;
     }
 
     //Mostrar información del libro
@@ -118,7 +124,7 @@ public class Libro {
         info.append("Año de Publicación: ").append(añoPublicacion > 0 ? añoPublicacion : "No proporcionado").append("\n");
         info.append("Idioma: ").append(idioma.equals("Idioma no especificado") ? "No proporcionado" : idioma).append("\n");
         info.append("Páginas: ").append(paginas > 0 ? getPaginas() : "No proporcionado").append("\n");
-        info.append("Leído: ").append(getLeido()).append("\n");
+        info.append("Leído: ").append(leido ? "Sí" : "No").append("\n");
         info.append("Antigüedad: ").append(getAntiguedad()).append("\n");
         return info.toString();
     }
@@ -135,28 +141,27 @@ public class Libro {
         }
     }
 
-    public void marcarComoLeido() {
-        this.leido = true;
+    public void marcarComoLeido(boolean leido) {
+        this.leido = leido;
     }
 
-    public void marcarComoNoLeido(){
-        this.leido = false;
-    }
-
-    //Practicando el method overloading:
-    //Actualizar únicamente el título
-    public void actualizarDatos(String nuevoTitulo) {
-        this.titulo = nuevoTitulo;
-    }
-    //Actualizar tanto título como autor
-    public void actualizarDatos(String nuevoTitulo, String nuevoAutor) {
-        this.titulo = nuevoTitulo;
-        this.autor = nuevoAutor;
-    }
-    //Actualizar título, autor, número de páginas
-    public void actualizarDatos(String nuevoTitulo, String nuevoAutor, int nuevasPaginas) {
-        this.titulo = nuevoTitulo;
-        this.autor = nuevoAutor;
-        this.paginas = nuevasPaginas;
+    public void actualizarInformacion(String nuevoTitulo, String nuevoAutor, String nuevoIsbn, String nuevoGenero, String nuevaEditorial, int nuevoAñoPublicacion, String nuevoIdioma, int nuevasPaginas){
+        titulo = nuevoTitulo;
+        autor = nuevoAutor;
+        isbn = nuevoIsbn;
+        genero = nuevoGenero;
+        editorial =nuevaEditorial;
+        if(nuevoAñoPublicacion > 0 && nuevoAñoPublicacion <= LocalDateTime.now().getYear()){
+            añoPublicacion = nuevoAñoPublicacion;
+        }else{
+            añoPublicacion = 0;
+        }
+        idioma = nuevoIdioma;
+        if(nuevasPaginas > 0){
+            paginas = nuevasPaginas;
+        }else{
+            paginas = 0;
+        }
+        //Para el booleano leido ya tenemos el metodo marcarComoLeido() o el método marcarComoNoLeido()
     }
 }
